e0415cd14b8f70e04153b7f9cd8cda839b337380
stupendous-web <topher@stupendousweb.com>
2022-07-09 13:58:50 -0700

    Think twice, code once.

--- redux/reducers.js
+++ redux/reducers.js
@@ -1,12 +1,8 @@
 import moment from "moment";
 
-console.log(moment().hour());
-
 const initialState = {
   daytime: moment().hour() < 16 ? true : false,
 };
-
-console.log(initialState.daytime);
 
 export default function reducers(state = initialState, action) {
   switch (action.type) {
