7d663eaf52677494a5f927388769c2e5bac419a4
stupendous-web <topher@stupendousweb.com>
2022-07-09 11:28:16 -0700

    Think twice, code once.

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import uikit from "uikit";
 import "../styles/uikit/uikit.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -5,8 +6,12 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
+import { init } from "@socialgouv/matomo-next";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    init({ url: "https://analytics.stupendousweb.com/", siteId: 2 });
+  }, []);
   return (
     <Provider store={store}>
       <Component {...pageProps} />
